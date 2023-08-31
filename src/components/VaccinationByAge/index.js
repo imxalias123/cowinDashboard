a291de15f51d3b469de066df84fe1bc6c77e2d82
sailaxmi <sailaxmikomirineni@gmail.com>
2023-08-31 16:29:41 +0000

    solution

--- src/components/VaccinationByAge/index.js
+++ src/components/VaccinationByAge/index.js
@@ -1,6 +1,6 @@
 // Write your code here
 // Write your code here
-import {PieChart, Pie, ResponsiveContainer, Cell, Legend} from 'recharts'
+import {PieChart, Pie, Cell, Legend} from 'recharts'
 
 const VaccinationByAge = props => {
   const {data} = props
@@ -8,25 +8,23 @@ const VaccinationByAge = props => {
     <div className="sub-parts">
       <h1 className="Coverage-heading">Vaccination by Age</h1>
       <div className="center">
-        <ResponsiveContainer width="100%" height={300}>
-          <PieChart>
-            <Pie
-              cx="50%"
-              cy="40%"
-              data={data}
-              startAngle={0}
-              endAngle={360}
-              innerRadius="0%"
-              outerRadius="70%"
-              dataKey="count"
-            >
-              <Cell name="18-44" fill="#2d87bb" />
-              <Cell name="44-60" fill="#a3df9f" />
-              <Cell name="Above 60" fill="#64c2a6" />
-            </Pie>
-            <Legend iconType="circle" verticalAlign="bottom" />
-          </PieChart>
-        </ResponsiveContainer>
+        <PieChart width="100%" height={300}>
+          <Pie
+            cx="50%"
+            cy="40%"
+            data={data}
+            startAngle={0}
+            endAngle={360}
+            innerRadius="0%"
+            outerRadius="70%"
+            dataKey="count"
+          >
+            <Cell name="18-44" fill="#2d87bb" />
+            <Cell name="44-60" fill="#a3df9f" />
+            <Cell name="Above 60" fill="#64c2a6" />
+          </Pie>
+          <Legend iconType="circle" verticalAlign="bottom" />
+        </PieChart>
       </div>
     </div>
   )
--- src/components/VaccinationByGender/index.js
+++ src/components/VaccinationByGender/index.js
@@ -1,34 +1,35 @@
 // Write your code here
-import {PieChart, Pie, ResponsiveContainer, Cell, Legend} from 'recharts'
+import {PieChart, Pie, Cell, Legend} from 'recharts'
 
 const VaccinationByGender = props => {
   const {data} = props
   return (
     <div className="sub-parts">
       <h1 className="Coverage-heading">Vaccination by gender</h1>
-      <ResponsiveContainer width="100%" height={300}>
-        <PieChart
-          margin={{
-            top: 45,
-          }}
+
+      <PieChart
+        width="100%"
+        height={300}
+        margin={{
+          top: 45,
+        }}
+      >
+        <Pie
+          cx="50%"
+          cy="30%"
+          data={data}
+          startAngle={0}
+          endAngle={180}
+          innerRadius="40%"
+          outerRadius="70%"
+          dataKey="count"
         >
-          <Pie
-            cx="50%"
-            cy="30%"
-            data={data}
-            startAngle={0}
-            endAngle={180}
-            innerRadius="40%"
-            outerRadius="70%"
-            dataKey="count"
-          >
-            <Cell name="Male" fill=" #f54394" />
-            <Cell name="Female" fill="#5a8dee" />
-            <Cell name="Others" fill=" #2cc6c6" />
-          </Pie>
-          <Legend iconType="circle" verticalAlign="bottom" />
-        </PieChart>
-      </ResponsiveContainer>
+          <Cell name="Male" fill=" #f54394" />
+          <Cell name="Female" fill="#5a8dee" />
+          <Cell name="Others" fill=" #2cc6c6" />
+        </Pie>
+        <Legend iconType="circle" verticalAlign="bottom" />
+      </PieChart>
     </div>
   )
 }
